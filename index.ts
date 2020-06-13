5e82706da57ac3819661532e9ed0c9cb9b0b0fe0
Anand Chowdhary <mail@anandchowdhary.com>
2020-06-13 22:38:44 +0530

    :construction: Add default value

--- index.ts
+++ index.ts
@@ -2,17 +2,20 @@ import { exec as _exec } from "child_process";
 import { join } from "path";
 import ora from "ora";
 import slugify from "@sindresorhus/slugify";
-import {} from "inquirer";
+import { prompt } from "inquirer";
 
 export const carpent = async (name: string, repo: string) => {
   const spinner = ora("Cloning git repository").start();
+  const slug = slugify(name);
 
   // Clone the repo
-  const result = await exec(`git clone ${repo} ${name}`);
-  const path = join(".", name);
+  await exec(`git clone ${repo} ${slug}`);
+  const path = join(".", slug);
+
+  // Find .carpentrc
 
   // Return the result
-  spinner.succeed(`Project ${path} is ready`);
+  spinner.succeed(`Project ${slug} is ready in ./${path}`);
   return { path };
 };
 
@@ -23,5 +26,16 @@ const exec = (command: string) =>
       resolve(stdout);
     });
   });
+
+const DEFAULT = {
+  questions: [
+    {
+      type: "input",
+      message: "Project name",
+      files: ["package.json"],
+      find: "carpent",
+    },
+  ],
+};
 
 carpent("anand", "http://github.com/AnandChowdhary/carpent");
