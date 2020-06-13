fc7fb6cf1aab6db96ef79325686f20d10cb6bf64
Anand Chowdhary <mail@anandchowdhary.com>
2020-06-13 22:16:11 +0530

    :construction: Add basic structure

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-export const nodeTs = () => true;
+import { exec as _exec } from "child_process";
+import { join } from "path";
+
+export const carpent = async (name: string, repo: string) => {
+  // Clone the repo
+  const result = await exec(`git clone ${repo} ${name}`);
+  const path = join(".", name);
+  console.log({ path });
+};
+
+const exec = (command: string) =>
+  new Promise((resolve, reject) => {
+    _exec(command, (error, stdout) => {
+      if (error) return reject(error);
+      resolve(stdout);
+    });
+  });
+
+carpent("anand", "http://github.com/AnandChowdhary/carpent");
