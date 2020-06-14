d9f1917e807544a783768295866bac56e70f380b
Anand Chowdhary <mail@anandchowdhary.com>
2020-06-14 13:06:41 +0530

    :sparkles: Support license

--- index.ts
+++ index.ts
@@ -39,35 +39,80 @@ export const carpent = async (
     config = await readJson(join(path, ".carpentrc"));
 
   // Ask questions
+  const allQuestions = [
+    ...config.questions,
+    {
+      name: "license",
+      type: "input",
+      message: "License",
+      default: "MIT License",
+    },
+    {
+      name: "licenseName",
+      type: "input",
+      message: "Name in license (usually full name)",
+      default: slug,
+    },
+  ];
   const answers: { [index: string]: string } = await inquirer.prompt(
-    config.questions
+    allQuestions
   );
 
   // Update values
   spinner.text = "Updating values";
   spinner.start();
-  for await (const question of config.questions) {
-    for await (const file of question.files) {
-      const VAL = question.replace
-        ? (question.replace as string).replace(
-            new RegExp("$VALUE", "g"),
-            answers[question.name]
-          )
-        : answers[question.name];
-      let fileContents = await readFile(join(slug, file), "utf8");
-      if (question.find)
-        fileContents = fileContents.replace(
-          new RegExp(question.find, "g"),
-          VAL
-        );
+  for await (const question of allQuestions) {
+    const VAL = question.replace
+      ? (question.replace as string).replace(
+          new RegExp("$VALUE", "g"),
+          answers[question.name]
+        )
+      : answers[question.name];
 
-      // Support JSON key changes
-      if (question.jsonKey) {
-        const jsonContent: any = JSON.parse(fileContents);
-        jsonContent[question.jsonKey] = VAL;
-        fileContents = JSON.stringify(jsonContent, null, 2);
+    // Update each file
+    for await (const file of question.files ?? []) {
+      if (await pathExists(join(slug, file))) {
+        let fileContents = await readFile(join(slug, file), "utf8");
+        if (question.find)
+          fileContents = fileContents.replace(
+            new RegExp(question.find, "g"),
+            VAL
+          );
+
+        // Support JSON key changes
+        if (question.jsonKey) {
+          const jsonContent: any = JSON.parse(fileContents);
+          jsonContent[question.jsonKey] = VAL;
+          fileContents = JSON.stringify(jsonContent, null, 2);
+        }
+        await writeFile(join(slug, file), fileContents);
       }
-      await writeFile(join(slug, file), fileContents);
+    }
+
+    // Update license
+    if (question.name === "license") {
+      const licenseText = await readFile(
+        join(".", "assets", "licenses", `${VAL}.txt`),
+        "utf8"
+      );
+
+      // Create LICENSE file
+      await writeFile(
+        join(slug, "LICENSE"),
+        licenseText
+          .replace("$NAME", answers.licenseName)
+          .replace("$YEAR", new Date().getFullYear().toString())
+      );
+
+      // Update license in package.json
+      if (await pathExists(join(slug, "package.json"))) {
+        let packageJson = await readJson(join(slug, "package.json"));
+        packageJson.license = VAL;
+        await writeFile(
+          join(slug, "package.json"),
+          JSON.stringify(packageJson, null, 2)
+        );
+      }
     }
   }
 
@@ -100,15 +145,22 @@ const DEFAULT = {
       files: ["package.json"],
       jsonKey: "name",
     },
-    {
-      name: "license",
-      type: "input",
-      message: "License",
-      files: ["package.json"],
-      jsonKey: "license",
-      default: "MIT",
-    },
   ],
+};
+
+export const LICENSES = {
+  "Apache-2.0": "Apache 2.0",
+  "BSD-3-Clause": "BSD 3-clause",
+  "CC-BY-SA-4.0": "Creative Commons Attribution Share-Alike (CC BY SA 4.0)",
+  "GPL-3.0": "GPL 3.0",
+  MIT: "MIT License",
+  UNLICENSED: "No open-source license (private and proprietary)",
+  "BSD-2-Clause": "BSD 2-clause",
+  "CC-BY-4.0": "Creative Commons Attribution (CC BY 4.0)",
+  "CC0-1.0": "Creative Commons Zero (CC0)",
+  ISC: "ISC License",
+  "MPL-2.0": "Mozilla Public License 2.0",
+  Unlicense: "The Unlicense",
 };
 
 carpent("http://github.com/AnandChowdhary/carpent");
